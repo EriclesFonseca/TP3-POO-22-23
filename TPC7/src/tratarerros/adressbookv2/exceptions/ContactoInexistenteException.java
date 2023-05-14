e778673dbb98bdb24d35a5025f07cfc26d343984
EriclesFonseca <117843246+EriclesFonseca@users.noreply.github.com>
2023-05-14 17:40:48 -0100

    Add files via upload

---
+++ TPC7/src/tratarerros/adressbookv2/exceptions/ContactoInexistenteException.java
@@ -0,0 +1,16 @@
+package tratarerros.adressbookv2.exceptions;
+
+public class ContactoInexistenteException extends Exception{
+    private String key;
+
+    public ContactoInexistenteException(String key){
+        this.key = key;
+    }
+    public String getKey(){
+        return key;
+    }
+
+    public String toString(){
+        return "Nenhum contacto encontrado para a chave " + key;
+    }
+}
---
+++ TPC7/src/tratarerros/adressbookv2/ui/PalavrasComando.java
@@ -0,0 +1,12 @@
+package tratarerros.adressbookv2.ui;
+
+public enum PalavrasComando {
+    adi,
+    alterar,
+    buscar,
+    remover,
+    procurar,
+    listar,
+    help,
+    sair,
+}
